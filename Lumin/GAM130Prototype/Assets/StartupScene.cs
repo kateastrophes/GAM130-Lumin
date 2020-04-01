00f043f7f6ba73a2274c836942b4b04518ccb335
Joe Broughton <jodeph.broughton@gmail.com>
2020-04-01 13:34:06 +0100

    Build now works

---
+++ Lumin/GAM130Prototype/Assets/StartupScene.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartupScene : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.None;
+    }
+
+}
