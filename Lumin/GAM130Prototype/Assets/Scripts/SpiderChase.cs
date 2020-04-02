ce586307e0d3765e3a93f70951dfd5dd5b406165
mcgeecaitlin <cm232388@falmouth.ac.uk>
2020-04-02 09:32:42 +0100

    Made chasespider.prefab and made final changes to SpiderChase.cs and SpiderPatrol.cs
    - To set the radius for chasing and attacking, enter the values in the inspector. DO NOT USE THE SCRIPT TO SET THESE VALUES.
    - For the chase spider set the player as the target in the inspector.

--- Lumin/GAM130Prototype/Assets/Scripts/SpiderChase.cs
+++ Lumin/GAM130Prototype/Assets/Scripts/SpiderChase.cs
@@ -20,41 +20,52 @@ public class SpiderChase : MonoBehaviour
 
     void Update()
     {
-        bool isPlayerClose = IsPlayerClose();
+        bool canAttackPlayer = CanAttackPlayer();
         if (!anim.GetCurrentAnimatorStateInfo(0).IsName("DigEmerge")) inPLay = true;
 
         if (inPLay)
         {
             Vector3 newTargetPostition = new Vector3(target.position.x, this.transform.position.y, target.position.z);
             agent.destination = newTargetPostition;
+            Debug.Log("Is chasing the player.");
             anim.SetBool("WalkForwards", true);
             if (attacking)
             {
                 anim.SetBool("Attack", false);
                 attacking = false;
             }
-            if (isPlayerClose)
+            if (canAttackPlayer)
             {
                 AttackPlayer();
             }
         }
 
     }
-
-    bool IsPlayerClose()
+    bool CanAttackPlayer()
     {
+        // For each object that is found within a sphere surrounding the spider, check if the object is the player.
         Collider[] nearbyObjects = Physics.OverlapSphere(transform.position, attackRadius);
         foreach (Collider item in nearbyObjects)
         {
-            Debug.Log(item);
-
             if (item.CompareTag("Player"))
             {
-                target = item.transform;
-                Debug.Log("Player near is true.");
                 return true;
             }
         }
         return false;
+    }
+
+    void AttackPlayer()
+    {
+        // Set the animation for the spider to attack.
+        attacking = true;
+        anim.SetBool("Attack", true);
+        Debug.Log("Player being attacked");
+    }
+    private void OnDrawGizmos()
+    {
+        // Radius for attacking the player.
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, attackRadius);
     }
 }
