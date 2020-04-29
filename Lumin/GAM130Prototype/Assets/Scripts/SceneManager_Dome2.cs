dffc662f1ab65c35968069135214a73d3e5795f6
Robbie <robomooer@gmail.com>
2020-04-29 21:17:16 +0100

    Merged all new changes in Andrea's scene

--- Lumin/GAM130Prototype/Assets/Scripts/SceneManager_Dome2.cs
+++ Lumin/GAM130Prototype/Assets/Scripts/SceneManager_Dome2.cs
@@ -4,8 +4,13 @@ using UnityEngine;
 
 public class SceneManager_Dome2 : MonoBehaviour
 {
+    public InteractableItem m_keycard;
     void Start()
     {
+        if (PlayerInventory.obj().has(PlayerInventory.items.KEYCARD_RED)) {
+            Destroy(m_keycard.m_parent);
+        }
+
         //set entrance level spawnpoint
         SpawnpointManager.s_spawn[(int)SpawnpointManager.scenes.ENTRANCE] = 2;
     }
